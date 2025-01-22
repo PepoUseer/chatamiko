bda8af39b5bffe3d42e1caa7f45b5812130b91f8
PepoUseer <113388350+PepoUseer@users.noreply.github.com>
2025-01-22 15:34:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sk_SK">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<title>Vtipné hádanky s.r.o.</title>
+</head>
+
+<body>
+	<h1>Chata Miko</h1>
+	<?
+	echo "ahoj slnko";
+	?>
+</body>
+
+</html>
