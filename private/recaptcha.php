05481ac036ebf45c92d3e0af872f3efc3d76bee5
Maroš Bartoš <113388350+PepoUseer@users.noreply.github.com>
2025-07-19 00:40:33 +0200

    Update recaptcha.php

--- private/recaptcha.php
+++ private/recaptcha.php
@@ -6,22 +6,17 @@ function verifyRecaptcha()
     $envFile = __DIR__ . '/.env';
     if (file_exists($envFile)) {
         $envVariables = parse_ini_file($envFile);
-        // Prihlasovacie údaje do databázy
         $secretKey = $envVariables['RECAPTCHA_SECRET'];
     } else {
-        // Ak chýba konfiguračný súbor, môžeme považovať overenie za neúspešné
         echo "chýbajúci konfiguračný kód pre recaptchu.";
         return false;
     }
 
-    // 1) Získame token, ktorý poslal klient
     $captchaResponse = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : '';
 
     if (!$captchaResponse) {
         return false;
     }
-
-    // 2) Zavoláme Google reCAPTCHA API
 
     $verifyUrl = "https://www.google.com/recaptcha/api/siteverify"
         . "?secret=" . urlencode($secretKey)
@@ -34,7 +29,6 @@ function verifyRecaptcha()
 
     $captchaObj = json_decode($responseData, true); // decode JSON -> pole
 
-    // Pridajte dočasné logovanie alebo var_dump:
     error_log("RECAPTCHA RESPONSE: " . print_r($captchaObj, true));
 
     $captchaResponse = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : '';
